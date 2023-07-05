9a73678accb98af238c54d7bc9c8b00d39a38adb
BrydenIsNotSmart <bryden@vitaldevs.org>
2023-07-04 20:58:37 -0500

    Co-authored-by: Chase <palentier.business@gmail.com>

--- src/index.js
+++ src/index.js
@@ -12,11 +12,15 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-
 fs.readdirSync(path.join(__dirname, "routes/v1")).forEach((route) => {
-    app.use(`/api/v1/${route.replace(".js", "")}`, require(`./routes/v1/${route}`).default);
+    app.use(`/api/v1/${route.replace(".js", "")}`, require(`./routes/v1/${route}`));
 })
 
-const server = app.listen(process.env.PORT, () => {
-    
-})
+app.get("/api/v1", (req, res) => res.send("Hello World"))
+
+//mongoose.connect(process.env.MONGOURI).then(async () => {
+    //console.info(`[INFO] Connected to MongoDB.`)
+     app.listen(process.env.PORT, () => {
+         console.info(`[INFO] Running on port ${process.env.PORT}.`)
+     })
+//})
