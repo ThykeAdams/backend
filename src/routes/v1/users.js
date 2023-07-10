b26cdaacbfbf1a398f3bf0d09772ad3a59e0608f
BrydenIsNotSmart <bryden@vitaldevs.org>
2023-07-10 02:55:02 -0500

    Co-authored-by: Chase <palentier.business@gmail.com>

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const { verifyToken } = require("../../util/middleware");
 const router = express.Router();
 
 router.get("/", (req, res) => {
     res.send("Please help")
+})
+
+router.get("/:id", verifyToken, (req, res) => {
+
 })
 
 module.exports = router;
